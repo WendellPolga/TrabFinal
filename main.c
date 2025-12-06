3938630c4b323fa7d6a5215f0d2d90c445e3542b
Wendell Polga <112591717+WendellPolga@users.noreply.github.com>
2025-12-06 17:39:52 -0300

    alterações necessárias

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
-#include "biblioteca.h"
+#include "biblioteca.h" 
 
 int menuPrincipal() {
     int opcao;
     printf("\n--- Sistema de Gerenciamento da Biblioteca ---\n");
+    printf("--- ALUNOS ---\n");
     printf("1. Inserir Novo Aluno\n");
     printf("2. Remover Aluno\n");
     printf("3. Alterar Dados de Aluno\n");
     printf("4. Apresentar Todos os Alunos\n");
-    printf("5. Cadastrar Livros\n"); 
-    printf("6. Cadastrar Turmas\n"); 
+    printf("--- TURMAS E LIVROS ---\n");
+    printf("5. Cadastrar Nova Turma\n");
+    printf("6. Apresentar Turmas Cadastradas\n");
+    printf("7. Cadastrar Novo Livro\n");
+    printf("8. Apresentar Catálogo de Livros\n");
     printf("0. Sair\n");
     printf("Escolha uma opcao: ");
     
@@ -22,32 +26,26 @@ int menuPrincipal() {
 
 int main() {
     inicializarDados(); 
+
     int opcao;
 
     do {
         opcao = menuPrincipal(); 
-      
+
         switch (opcao) {
-            case 1:
-                inserirAluno(); 
-                break;
-            case 2:
-                removerAluno(); 
-                break;
-            case 3:
-                alterarAluno(); 
-                break;
-            case 4:
-                apresentaAlunos(); 
-                break;
-            case 5:
-                cadastroSimples("LIVROS"); 
-                break;
-            case 6:
-                cadastroSimples("TURMAS"); 
-                break;
+            case 1: inserirAluno(); break; 
+            case 2: removerAluno(); break;
+            case 3: alterarAluno(); break; 
+            case 4: apresentaAlunos(); break; 
+            
+            case 5: inserirTurma(); break;
+            case 6: apresentaTurmas(); break; 
+            
+            case 7: inserirLivro(); break;
+            case 8: apresentaLivros(); break; 
+
             case 0:
-                printf("\nEncerrando o sistema! Obrigado por usar o programa.\n");
+                printf("\nEncerrando o Sistema.\n");
                 break;
             default:
                 printf("\n[ERRO] Opcao invalida. Tente novamente.\n");
