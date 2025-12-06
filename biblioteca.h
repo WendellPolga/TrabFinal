267c3534f7d0a7a4084b392a055132e54479d86f
Wendell Polga <112591717+WendellPolga@users.noreply.github.com>
2025-12-06 17:21:51 -0300

    alteração em turmas e livros
    Added new structures for Turma and Livro, and updated Aluno structure.

--- biblioteca.h
+++ biblioteca.h
@@ -6,23 +6,40 @@
 #include <string.h>
 
 #define max_alunos 100 
+#define max_turmas 20
+#define max_livros 500
 #define max_livros_por_aluno 1 
 
 typedef struct {
     int matricula;
     char nome[50];
-    char turma[10];
-    int livro_emprestado;
+    char codigo_turma[10];
+    int id_livro_emprestado;
 } Aluno;
 
+typedef struct {
+    char codigo[10];
+    char nome_curso[40];
+    int limite_alunos;
+} Turma;
+
+typedef struct {
+    int id;
+    char titulo[60];
+    char autor[50];
+    int qtd_estoque;
+} Livro;
+
 void inicializarDados();
+
 void apresentaAlunos();
 void inserirAluno();
 void removerAluno();
 void alterarAluno();
-void cadastroSimples(const char *tipo);
+int buscarPosicaoAluno(int matricula_busca);
 
-int buscarPosicao(int matricula_busca);
-int buscarPorNome(const char *nome_busca);
+void inserirLivro();
+void apresentaLivros();
+int buscarPosicaoLivro(int id_busca);
 
 #endif
